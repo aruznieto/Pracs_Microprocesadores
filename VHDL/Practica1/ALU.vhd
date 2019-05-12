1de2e82dc30de74d7b046e76d455f3a01322fab6
Andrés Ruz Nieto <andres.ruz@edu.upct.es>
2019-05-12 18:58:21 +0200

    Practica 1

--- VHDL/Practica1/ALU.vhd
+++ VHDL/Practica1/ALU.vhd
@@ -1,11 +1,11 @@
 ----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
+-- Company: 	UPCT - SDBM
+-- Engineer:	Andrés Ruz Nieto y Diego Ismael Antolinos García
 -- 
 -- Create Date:    18:30:11 05/12/2019 
--- Design Name: 
+-- Design Name: 	 
 -- Module Name:    ALU - Behavioral 
--- Project Name: 
+-- Project Name:   Diseño de una ALU
 -- Target Devices: 
 -- Tool versions: 
 -- Description: 
@@ -32,19 +32,25 @@ use IEEE.STD_LOGIC_1164.ALL;
 entity ALU is
     Port ( clk : in  STD_LOGIC;
            reset : in  STD_LOGIC;
-           A_in : in  STD_LOGIC;
-           A_out : in  STD_LOGIC;
-           OP_in : in  STD_LOGIC;
-           A_out : out  STD_LOGIC;
-           B_out : out  STD_LOGIC;
-           TipoOP_out : out  STD_LOGIC;
-           SALIDA_ALU : out  STD_LOGIC);
+           A_in : in  STD_LOGIC_VECTOR (7 DOWNTO 0);
+           A_out : in  STD_LOGIC_VECTOR (7 DOWNTO 0);
+           OP_in : in  STD_LOGIC_VECTOR (4 DOWNTO 0);
+           A_out : out  STD_LOGIC_VECTOR (7 DOWNTO 0);
+           B_out : out  STD_LOGIC_VECTOR (7 DOWNTO 0);
+           TipoOP_out : out  STD_LOGIC_VECTOR (1 DOWNTO 0);
+           SALIDA_ALU : out  STD_LOGIC_VECTOR (7 DOWNTO 0));
 end ALU;
 
 architecture Behavioral of ALU is
 
 begin
-
+-- REGISTRO OP_IN
+	PROCESS(clk)
+		BEGIN
+			IF(clk´EVENT AND clk='1') THEN 
+				OP_in <= OP;
+			END IF
+	END PROCESS
 
 end Behavioral;
 
