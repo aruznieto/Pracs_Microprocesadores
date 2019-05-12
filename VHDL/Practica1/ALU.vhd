4fb7b91d3dbbed55bf7abc7342f2278a8325a811
Andrés Ruz Nieto <andres.ruz@edu.upct.es>
2019-05-12 19:18:30 +0200

    Practica 1

--- VHDL/Practica1/ALU.vhd
+++ VHDL/Practica1/ALU.vhd
@@ -1,11 +1,11 @@
 ----------------------------------------------------------------------------------
 -- Company: 	UPCT - SDBM
--- Engineer:	Andrés Ruz Nieto y Diego Ismael Antolinos García
+-- Engineer:	Andres Ruz Nieto y Diego Ismael Antolinos Garcia
 -- 
 -- Create Date:    18:30:11 05/12/2019 
 -- Design Name: 	 
 -- Module Name:    ALU - Behavioral 
--- Project Name:   Diseño de una ALU
+-- Project Name:   ALU Design
 -- Target Devices: 
 -- Tool versions: 
 -- Description: 
@@ -33,11 +33,15 @@ entity ALU is
     Port ( clk : in  STD_LOGIC;
            reset : in  STD_LOGIC;
            A_in : in  STD_LOGIC_VECTOR (7 DOWNTO 0);
-           A_out : in  STD_LOGIC_VECTOR (7 DOWNTO 0);
+           DatoA : out  STD_LOGIC_VECTOR (7 DOWNTO 0);
+           B_in : in  STD_LOGIC_VECTOR (7 DOWNTO 0);
+           DatoB : out  STD_LOGIC_VECTOR (7 DOWNTO 0);
            OP_in : in  STD_LOGIC_VECTOR (4 DOWNTO 0);
+           OP : out  STD_LOGIC_VECTOR (4 DOWNTO 0);
            A_out : out  STD_LOGIC_VECTOR (7 DOWNTO 0);
            B_out : out  STD_LOGIC_VECTOR (7 DOWNTO 0);
            TipoOP_out : out  STD_LOGIC_VECTOR (1 DOWNTO 0);
+           TipoOP : in  STD_LOGIC_VECTOR (1 DOWNTO 0);
            SALIDA_ALU : out  STD_LOGIC_VECTOR (7 DOWNTO 0));
 end ALU;
 
@@ -47,10 +51,35 @@ begin
 -- REGISTRO OP_IN
 	PROCESS(clk)
 		BEGIN
-			IF(clk´EVENT AND clk='1') THEN 
-				OP_in <= OP;
-			END IF
-	END PROCESS
+			IF(clk'EVENT AND clk='1') THEN 
+				OP <= OP_in;
+			END IF;
+	END PROCESS;
+	
+-- REGISTRO A_IN
+	PROCESS(clk)
+		BEGIN
+			IF(clk'EVENT AND clk='1') THEN 
+				DatoA <= A_in;
+			END IF;
+	END PROCESS;
+	
+-- REGISTRO B_IN
+	PROCESS(clk)
+		BEGIN
+			IF(clk'EVENT AND clk='1') THEN 
+				DatoB <= B_in;
+			END IF;
+	END PROCESS;
+	
+-- REGISTRO TIPO OP
+	PROCESS(clk)
+		BEGIN
+			IF(clk'EVENT AND clk='1') THEN 
+				TipoOP_out <= TipoOP;
+			END IF;
+	END PROCESS;
+	
 
 end Behavioral;
 
